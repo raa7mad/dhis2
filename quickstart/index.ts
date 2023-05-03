a3879f22b7562a62068f25384d782e91e6557e22
Daniel Lauzon <daniel.lauzon@gmail.com>
2023-05-03 13:07:45 -0400

    rm de stack

--- quickstart/index.ts
+++ quickstart/index.ts
@@ -5,7 +5,31 @@ import * as gcp from "@pulumi/gcp";
 const bucket = new gcp.storage.Bucket("my-bucket", {
   location: "northamerica-northeast1",
   uniformBucketLevelAccess: true, // Enable Uniform Bucket-Level Access
+  website: {
+    mainPageSuffix: "index.html",
+  },
+});
+
+const bucketIAMBinding = new gcp.storage.BucketIAMBinding(
+  "my-bucket-IAMBinding",
+  {
+    bucket: bucket.name,
+    role: "roles/storage.objectViewer",
+    members: ["allUsers"],
+  }
+);
+
+const bucketObject = new gcp.storage.BucketObject("index.html", {
+  bucket: bucket.name,
+  contentType: "text/html",
+  source: new pulumi.asset.FileAsset("index.html"),
 });
 
 // Export the DNS name of the bucket
 export const bucketName = bucket.url;
+export const bucketEndpoint = pulumi.concat(
+  "http://storage.googleapis.com/",
+  bucket.name,
+  "/",
+  bucketObject.name
+);
