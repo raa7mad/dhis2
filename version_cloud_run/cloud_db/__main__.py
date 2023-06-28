c97ccc11757cd4066876441aadba91a77af1d09d
Jia Chang <jia.chang@phac-aspc.gc.ca>
2023-06-28 20:25:13 +0000

    add cloud run, it is working?

--- version_cloud_run/cloud_db/__main__.py
+++ version_cloud_run/cloud_db/__main__.py
@@ -1,7 +1,15 @@
 """A Google Cloud Python Pulumi program"""
 
 import pulumi
-from pulumi_gcp import sql, secretmanager
+from pulumi_gcp import sql, secretmanager, cloudrun, iam
+
+#define variable
+region_local = "northamerica-northeast1"
+project_local = "phx-01h28kjpmp42v8xh59v0qvfpxa"
+#docker image of dhis2
+docker_image = "dhis2/core:2.39.1.2"
+# TODO: save password as secret
+password = "mypassword"
 
 # Create a secret for the database password
 # db_password_secret = secretmanager.Secret("my-db-password",
@@ -21,7 +29,7 @@ from pulumi_gcp import sql, secretmanager
 instance = sql.DatabaseInstance(
     "dhis2-instance",
     database_version="POSTGRES_12",
-    region="northamerica-northeast1",
+    region = region_local,
     settings=sql.DatabaseInstanceSettingsArgs(
         tier="db-f1-micro",
     ),
@@ -31,9 +39,42 @@ instance = sql.DatabaseInstance(
 # Create a database in the Cloud SQL instance
 database = sql.Database("dhis-db",
     instance=instance.name,
-    name="DHIS2_db"
+    name="DHIS2_db",
+    project = project_local,
 )
 
+
+# cloud run to deploy dhis2 docker image, with connection to cloud database becuase cloud run is stateless
+service = cloudrun.Service('dhis2-service', 
+    location=region_local,
+    template=cloudrun.ServiceTemplateArgs(
+        spec=cloudrun.ServiceTemplateSpecArgs(
+            containers=[cloudrun.ServiceTemplateSpecContainerArgs(
+                image=docker_image,
+                envs=[
+                    cloudrun.ServiceTemplateSpecContainerEnvArgs(
+                        name='DATABASE_USER',
+                        value='myuser', # Specify your database username here
+                    ),
+                    cloudrun.ServiceTemplateSpecContainerEnvArgs(
+                        name='DATABASE_NAME',
+                        value= database.name , # Specify your database name here
+                    ),
+                    cloudrun.ServiceTemplateSpecContainerEnvArgs(
+                        name='DATABASE_PASSWORD',
+                        value=password, # Specify your database password here
+                    ),
+                ],
+            )],
+        )),
+    traffics=[cloudrun.ServiceTrafficArgs(
+        latest_revision=True,
+        percent=100,
+    )],
+)
+
+
 # Export the Cloud SQL instance connection name and database name
-pulumi.export("instance_connection_name", instance.connection_name)
 pulumi.export("database_name", database.name)
+pulumi.export("instance_connection_name", instance.connection_name)
+pulumi.export("service", service.statuses)
